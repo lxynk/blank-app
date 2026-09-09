25a09d51d82769281a6703e8b93550de11509964
lxynk <133401748+lxynk@users.noreply.github.com>
2026-09-09 12:15:01 +0000

    final version!

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,26 +5,21 @@ st.write(
     "I'm into  typographically pretty text rendering, I hope you too!"
 )
 
-# a = st.latex(r"\sum_{k=0}^{n-1} ar^k")
-# b = "different font size of this question compared to the text before"
-# c = "double spaces between words (can you find any?)"
-# d = st.latex(r"a^2")
-# e = "none of the above"
+a = r"$\sum_{k=0}^{n-1} ar^k$"
+b = "different font size of this question compared to the text before"
+c = "double spaces between words (can you find any?)"
+d = r"$a^2$"
+e = "none of the above"
 
 answer = st.radio(
     "Sharan, to check your sense of typographic beauty, guess what I find disturbing:",
-    options=[
-        st.latex(r"\sum_{k=0}^{n-1} ar^k"),
-        "different font size of this question compared to the text before",
-        "double spaces between words (can you find any?)",
-        st.latex(r"a^2"),
-        "none of the above",
-        ],
+    [a,b,d,c,e],
 )
 
 submitted = st.button("Guess")
-if submitted and answer == "none of the above":
-    st.write("Riiiiight!")
-    st.balloons()
-else:
-    st.write("Sigh...")
+if submitted:
+    if answer in [b,c]:
+        st.write("Riiiiight!")
+        st.balloons()
+    else:
+        st.write("Sigh...")
