768db87c8578a9848c6f596b20875a52322e2289
lxynk <133401748+lxynk@users.noreply.github.com>
2026-09-09 10:06:22 +0000

    add a question

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈🎈🎈 Just baloons and not math")
 st.write(
-    "No!"
+    "I'm really into colors, I hope you too! Guess which is my favorite?"
 )
+
+options = ["1+3", "9-5", "2^2"]
+st.radio(
+    "Sharan, what do you think is NOT my favorite color?",
+    key="colors",
+    options=options,
+)
+answer = st.select_box("Guess", options)
+if answer == "9-5":
+    st.write("Riiiiight!")
+else:
+    st.write("Riiiiight!")
