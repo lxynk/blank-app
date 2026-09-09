b20e6889e1dae090d1d36408502c98393a233df4
lxynk <133401748+lxynk@users.noreply.github.com>
2026-09-09 10:40:16 +0000

    totally new

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,17 +2,30 @@ import streamlit as st
 
 st.title("🎈🎈🎈 Just baloons and no math")
 st.write(
-    "I'm really into colors, I hope you too! Sharan, can you guess what is my favorite color?"
+    "I'm really into  typographically pretty text rendering, I hope you too!"
 )
 
-options = ["1+3", "9-5", "2^2"]
+a = st.latex(r"\sum_{k=0}^{n-1} ar^k")
+b = "different font size of this question compared to the text before"
+c = "double spaces between words (can you find any?)"
+d = st.latex(r"a^2")
+e = "none of the above"
+
+options = [
+    a,
+    b,
+    c,
+    d,
+    e]
 
 answer = st.radio(
-    "",
+    "Sharan, to check your sense of typographic beauty, guess what I find disturbing:",
     options=options,
 )
 
 submitted = st.button("Guess")
-if submitted and answer in options:
+if submitted and answer in [b,c]:
     st.write("Riiiiight!")
     st.balloons()
+elif submitted and answer not in [b,c]:
+    st.write("Sigh...")
